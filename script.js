290c66d6d8d1654520eb012aecb4473e269a5351
SABRIS.13 <sabriscon13@hotmail.com>
2023-04-14 15:42:53 -0700

    Establecer estilo de header y head

---
+++ script.js
@@ -0,0 +1,12 @@
+/* ===== Loader =====*/
+window.addEventListener('load', () => {
+    const contenedorLoader = document.querySelector('.container_load');
+    contenedorLoader.style.opacity = 0;
+    contenedorLoader.style.visibility = 'hidden';
+})
+
+/*===== Header =====*/
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('abajo', window.scrollY > 0);
+});
