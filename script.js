5f2e0ecf120b9a85116ab3847acb680499881820
SABRIS.13 <sabriscon13@hotmail.com>
2023-04-20 14:42:36 -0700

    Activacion de cambio de idioma: Ingles/Español

--- script.js
+++ script.js
@@ -74,20 +74,3 @@ window.addEventListener('scroll', () => {
         }
     });
 });
-
-/*===== Boton y función ir arriba =====*/
-window.onscroll = function() {
-    if (document.documentElement.scrollTop > 100) {
-        document.querySelector('.go-top-container').classList.add('show');
-    }
-    else {
-        document.querySelector('.go-top-container').classList.remove('show');
-    }
-}
-
-document.querySelector('.go-top-container').addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
